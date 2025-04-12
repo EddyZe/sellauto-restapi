c289efd2204156d4e4a46c742002fb41b53e27b2
zemae <qwe123>
2025-04-12 13:36:07 +0300

    добавление документации к моделям

--- src/main/java/ru/eddyz/sellautorestapi/controllers/ModelController.java
+++ src/main/java/ru/eddyz/sellautorestapi/controllers/ModelController.java
@@ -1,18 +1,28 @@
 package ru.eddyz.sellautorestapi.controllers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.eddyz.sellautorestapi.dto.ModelBaseDto;
+import ru.eddyz.sellautorestapi.dto.ModelsBaseDto;
 import ru.eddyz.sellautorestapi.mapper.ModelsBaseMapper;
 import ru.eddyz.sellautorestapi.service.ModelService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/models")
+@Tag(name = "Модели авто")
 public class ModelController {
 
 
@@ -21,16 +31,45 @@ public class ModelController {
 
 
     @GetMapping
+    @Operation(
+            summary = "Список моделей",
+            description = "Позволяет получить список созданных моделей",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ModelsBaseDto.class))
+                    )
+            }
+    )
     public ResponseEntity<?> getModels() {
         return ResponseEntity
-                .ok(modelService.findAll()
-                        .stream()
-                        .map(modelsBaseMapper::toDto)
-                        .toList());
+                .ok(ModelsBaseDto.builder()
+                        .models(modelService.findAll()
+                                .stream()
+                                .map(modelsBaseMapper::toDto)
+                                .toList())
+                        .build());
     }
 
-    @GetMapping("/{modelsId}")
-    public ResponseEntity<?> getModelById(@PathVariable("modelsId") Integer modelId) {
+    @GetMapping("/{modelId}")
+    @Operation(
+            summary = "Получить модель по ID",
+            description = "Позволяет получить модель по ID",
+            parameters = {
+                    @Parameter(name = "modelId", description = "ID модели")
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ModelBaseDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Ошибка",
+                            content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+                    )
+            }
+    )
+    public ResponseEntity<?> getModelById(@PathVariable("modelId") Integer modelId) {
         return ResponseEntity
                 .ok(modelsBaseMapper.toDto(modelService.findById(modelId)));
     }
